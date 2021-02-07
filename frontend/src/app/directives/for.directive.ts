dfa39b3982c21daa0b1d60559f54102a4f6464a4
mrcsabatini <mrcssbtn@gmail.com>
2021-02-07 10:30:04 -0300

    Directives style and for

---
+++ frontend/src/app/directives/for.directive.ts
@@ -0,0 +1,25 @@
+import {
+  Directive,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef
+} from '@angular/core';
+
+@Directive({
+  selector: '[myFor]'
+})
+export class ForDirective implements OnInit {
+  @Input('myForEm') numbers: number[];
+
+  constructor(
+    private container: ViewContainerRef,
+    private template: TemplateRef<any>
+  ) {}
+
+  ngOnInit(): void {
+    this.numbers.forEach((number) =>
+      this.container.createEmbeddedView(this.template, { $implicit: number })
+    );
+  }
+}
